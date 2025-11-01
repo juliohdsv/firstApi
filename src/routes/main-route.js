233ce0cc923490af9d66897a7e7a74a39c2ac5ba
Henrique <Henrique@M4.local>
2025-11-01 20:42:35 -0300

    feat(sing-up): Created route with users register

--- src/routes/main-route.js
+++ src/routes/main-route.js
@@ -1,18 +1,28 @@
 
 import { Router } from "express"
 
+const users = []
+
 export const mainRoute = Router()
 
-mainRoute.get("/test", (request, response)=> {
-  return response.status(200).send({ message: "OK" })
-})
-
- mainRoute.get("/", (request, response)=> {
-  return response.status(200).send({ message: "Seja bem-vindo" })
-})
-
- mainRoute.post("/login", (request, response)=> {
+ mainRoute.post("/sign-up", (request, response)=> {
   const { email, password } = request.body
 
-  return response.status(201).send({ body: { email, password} })
+  if(!email){
+    return response.status(400).send({ message: "Email required." })
+  }
+
+  if(!password){
+    return response.status(400).send({ message: "password required." })
+  }
+
+  const userExist = users.find(item => item.email == email)
+
+  if(userExist){
+    return response.status(409).send({ message: "User already exist." })
+  }
+
+  users.push({ email, password })
+
+  return response.status(201).send({ users })
 })
